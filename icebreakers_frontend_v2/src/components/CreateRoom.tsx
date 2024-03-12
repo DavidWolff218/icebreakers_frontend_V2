c7a2cb94f42863a05243598c090f70bc6e628e0c
doodleman218 <dcwolff218@gmail.com>
2024-03-12 13:53:32 -0500

    basic form set up, does make a post request and recieves data back. not set up on backend yet to complete request though

--- icebreakers_frontend_v2/src/components/CreateRoom.tsx
+++ icebreakers_frontend_v2/src/components/CreateRoom.tsx
@@ -1,38 +1,91 @@
 import { useEffect, useState } from "react";
 import { API_ROOT } from "../constants";
+import { render } from "@testing-library/react";
+import { hostname } from "os";
 
 const CreateRoom = () => {
+  const [roomName, setRoomName] = useState("");
+  const [hostName, setHostName] = useState("");
 
-  const [roomName, setRoomName] = useState("")
+  type RoomData = {
+    room_name: string;
+    username: string;
+  };
+
+  type ReqObj = {
+    method: string;
+    headers: {
+      "Content-Type": string;
+      Accept: string;
+    };
+    body: string;
+  };
 
   useEffect(() => {
     const fetchRoomCode = async () => {
-      try{
-        const resp = await fetch(`${API_ROOT}/rooms/room_code`)
-        if(resp.ok){
-          const data = await resp.json()
-          setRoomName(data)
+      try {
+        const resp = await fetch(`${API_ROOT}/rooms/room_code`);
+        if (resp.ok) {
+          const data = await resp.json();
+          setRoomName(data.room_name);
         } else {
-          console.log('Failed to fetch room name')
+          console.log("Failed to fetch room name");
         }
-      } catch(error){
-        console.error('Error fetching room name:', error)
+      } catch (error) {
+        console.error("Error fetching room name:", error);
       }
-    }
-    fetchRoomCode()
-  }, [])
+    };
+    fetchRoomCode();
+  }, []);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setHostName(event.target.value);
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const reqObj: ReqObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        room: { room_name: roomName, username: hostName } as RoomData,
+      }),
+    };
+
+    try {
+      const resp = await fetch(`${API_ROOT}/rooms`, reqObj);
+      if (!resp.ok) {
+        console.error(resp);
+      } 
+      const data = await resp.json();
+      console.log(data)
+    } catch {}
+  };
 
   const renderForm = (): JSX.Element => {
-    return(
-      <form>
-        
-      </form>
-    )
-  }
+    return (
+      <div>
+        Room Name
+        {roomName}
+        <form onSubmit={handleSubmit}>
+          <label>Enter Host Name</label>
+          <input type="text" value={hostName} onChange={handleChange} />
+          <button type="submit">
+          Create your Room
+        </button>
+        </form>
+      </div>
+    );
+  };
 
   return (
     <div>
       Here we can give you the room code and you can create your host name
+      {renderForm()}
     </div>
   );
 };
