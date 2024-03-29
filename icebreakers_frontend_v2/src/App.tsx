4b00affaf509face6ebb77eb13c0a6d6e55c156c
doodleman218 <dcwolff218@gmail.com>
2024-03-29 10:31:07 -0500

    changed routing logic to get rid of conditional, token does fetch and user is back in game, only problem is question and name dont show up

--- icebreakers_frontend_v2/src/App.tsx
+++ icebreakers_frontend_v2/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Link, Navigate } from "react-router-dom";
 import Home from "./Home";
 import Room from "./Room";
@@ -16,6 +16,42 @@ function App() {
     gameStarted: false,
   });
 
+
+  useEffect(() => {
+    const verifyToken = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        if (token) {
+          const resp = await fetch(`http://localhost:3000/verify_token`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: token,
+            },
+          });
+          if (resp.ok) {
+            const data = await resp.json();
+            if (data.room) {
+              console.log("token has been fetched", data)
+              //this can handle both the user and the host, but run into issues if the host has button when refreshes
+              setRoomInfo({
+                user: { userName: data.user.username, id: data.user.id },
+                roomName: data.room.room_name,
+                host: { id: data.room.host_id, hostName: data.room.host_name },
+                gameStarted: data.room.game_started,
+              });
+            }
+          } else {
+            console.error("Token is invalid or missing");
+          }
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    verifyToken();
+  }, []);
+
   const handleRoomData = (room: RoomData, user: UserData) => {
     setRoomInfo({
       user: { userName: user.username, id: user.id },
@@ -30,11 +66,12 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home handleRoomData={handleRoomData}/>} />
-          <Route path="/room/:roomId" element={roomInfo.user.id !== 0 ? (
+          <Route path="/room/:roomId" element={<Room roomInfo={roomInfo} />} />
+          {/* <Route path="/room/:roomId" element={roomInfo.user.id !== 0  || roomInfo.user.id !== undefined ? (
       <Room roomInfo={roomInfo} />
     ) : (
       <Navigate to="/" replace={true} /> // ***NOT SURE IF NEED THIS, EXPERIMENT LATER
-    )} />
+    )} /> */}
         </Routes>
       </Router>
     </div>
