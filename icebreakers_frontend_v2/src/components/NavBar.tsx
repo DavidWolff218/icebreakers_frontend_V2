e9d2bbf7fc1a4e9a7c617d9bcff467aeb13314ce
doodleman218 <dcwolff218@gmail.com>
2024-05-31 14:37:50 -0500

    added icons library and hamburger menu to nav

--- icebreakers_frontend_v2/src/components/NavBar.tsx
+++ icebreakers_frontend_v2/src/components/NavBar.tsx
@@ -1,4 +1,6 @@
 import { User } from "../types/types";
+//@ts-ignore
+import { UilBars } from '@iconscout/react-unicons'
 
 type NavProps = {
   user: User;
@@ -24,12 +26,25 @@ const NavBar = ({
   };
 
   return (
-    <nav className="absolute top-0 left-0 w-full flex items-center justify-center" >
-      <h2 className="text-4xl italic font-normal mt-7" >
+    <nav className="absolute top-0 left-0 w-full">
+    <div className="w-full flex justify-center relative">
+      <div className="absolute left-4 top-1/2 transform -translate-y-1/2">
+        <UilBars size="24" color="#000000" className="mt-7" />
+      </div>
+      <h2 className="text-4xl italic font-normal mt-7">
         IceBreakers
       </h2>
-    </nav>
+    </div>
+  </nav>
   );
 };
 
 export default NavBar;
+
+
+// <nav className="absolute top-0 left-0 w-full flex items-center justify-center" >
+// <UilBars size="24" color="#000000" />
+// <h2 className="text-4xl italic font-normal mt-7" >
+//   IceBreakers
+// </h2>
+// </nav>
