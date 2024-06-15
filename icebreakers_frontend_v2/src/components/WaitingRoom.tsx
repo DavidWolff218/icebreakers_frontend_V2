1af38b6df42e287f7c8074b1ce661732a61e9ebc
doodleman218 <dcwolff218@gmail.com>
2024-06-15 17:06:12 -0500

    tailwind updated waitingroom layout

--- icebreakers_frontend_v2/src/components/WaitingRoom.tsx
+++ icebreakers_frontend_v2/src/components/WaitingRoom.tsx
@@ -34,7 +34,7 @@ const WaitingRoom = ({
   const renderAllUsers = (allUsers: User[]): JSX.Element[] => {
     return allUsers.map((user: any) => {
       return (
-        <div className="w-[262px] border-b-[1.5px] border-gray py-1.5">
+        <div className="w-full border-b-[1.5px] border-gray py-1.5">
           <h3 key={user.id}>{user.username}</h3>
         </div>
       );
@@ -50,15 +50,21 @@ const WaitingRoom = ({
       return (
         <>
           <div className="bg-white w-[328px] h-[346px] flex flex-col items-center rounded-2xl">
-            <div className='flex items-center w-full justify-between'>
-              <div className="h-[40px] w-[40px]"/>
+            <div className="flex items-center w-full justify-between">
+              <div className="h-[40px] w-[40px]" />
               <h2 className="font-semibold text-[22px] leading-5 pt-3">
                 Players
               </h2>
-              <UilShareAlt onClick={handleInviteLink} className="pt-3 pr-4" size="40" />
+              <UilShareAlt
+                onClick={handleInviteLink}
+                className="pt-3 pr-4"
+                size="40"
+              />
             </div>
-            <div className="w-[262px] h-[227px] border-y-3 border-gray mt-2 overflow-y-auto overflow-x-hidden">
-              {renderAllUsers(allUsers)}
+            <div className="w-full px-8">
+              <div className="h-[227px] border-y-3 border-gray mt-1 overflow-y-auto overflow-x-hidden">
+                {renderAllUsers(allUsers)}
+              </div>
             </div>
             <div className="my-auto">{startButton()}</div>
           </div>
@@ -67,15 +73,23 @@ const WaitingRoom = ({
     } else {
       return (
         <>
-          <h2>
+          {/* <h2>
             The host, <span>{host.username}</span>, will start the game soon!
-          </h2>
-          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-            <div className="bg-white w-[328px] h-[284px] flex flex-col items-center rounded-2xl">
-              <h2 className="font-semibold text-[22px] tracking-[0.1px] leading-[20px] pt-[12px]">
+          </h2> */}
+          <div className="bg-white w-[328px] h-[284px] flex flex-col items-center rounded-2xl">
+            <div className="flex items-center w-full justify-between">
+              <div className="h-[40px] w-[40px]" />
+              <h2 className="font-semibold text-[22px] leading-5 pt-3">
                 Players
               </h2>
-              <div className="w-[262px] h-[227px] border-y-3 border-gray mt-2 overflow-y-auto overflow-x-hidden">
+              <UilShareAlt
+                onClick={handleInviteLink}
+                className="pt-3 pr-4"
+                size="40"
+              />
+            </div>
+            <div className="w-full px-8">
+              <div className="h-[227px] border-y-3 border-gray mt-1 overflow-y-auto overflow-x-hidden">
                 {renderAllUsers(allUsers)}
               </div>
             </div>
