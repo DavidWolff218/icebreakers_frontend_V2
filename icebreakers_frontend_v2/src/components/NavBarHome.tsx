d847cc750aefb073182991c232b40e5ed7d34358
doodleman218 <dcwolff218@gmail.com>
2024-06-20 14:05:57 -0500

    updating with no parent directory, all working

--- icebreakers_frontend_v2/src/components/NavBarHome.tsx
+++
@@ -1,9 +0,0 @@
-const NavBarHome = () => {
-  return (
-    <nav className="w-full flex justify-center items-center">
-      <h2 className="text-4xl italic font-normal mt-7">IceBreakers</h2>
-    </nav>
-  );
-};
-
-export default NavBarHome;
--- icebreakers_frontend_v2/src/modals/ErrorModal.tsx
+++
@@ -1,9 +0,0 @@
-type ErrorProps = {
-  errorText: string;
-};
-
-const ErrorModal = ({ errorText }: ErrorProps) => {
-  return <div>Here is the error message: {errorText}</div>;
-};
-
-export default ErrorModal;
